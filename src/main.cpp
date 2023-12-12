e116facd397ed31a0c490fc23b0b073369fa4464
filipton <filipton12@gmail.com>
2023-12-12 21:33:54 +0100

    wip: simple serial "terminal"

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,15 @@
 #include <LittleFS.h>
 // #include <EEPROM.h>
 
-void setup() {
-  Serial.begin(115200);
+String readSerialCommand();
 
+bool recordStarted = false;
+bool playbackStarted = false;
+bool showHelp = true;
+
+void setup() {
   LittleFS.begin();
+
   // EEPROM.begin(64);
 
   // File file = LittleFS.open("/test.txt", "a+");
@@ -15,7 +20,14 @@ void setup() {
   // }
 }
 
+void setup1() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
 void loop() {
+  
+
   // delay(1000);
 
   // FSInfo64 fsinfo;
@@ -32,4 +44,69 @@ void loop() {
   //   Serial.print(" / ");
   //   Serial.println(dir.fileSize());
   // }
+}
+
+void loop1() {
+  while (!Serial) {
+    showHelp = true;
+    delay(1);
+  }
+
+  if (showHelp) {
+    Serial.println("Execute function:");
+    Serial.println("- record [name] [baud]");
+    Serial.println("- play [name]");
+    Serial.println("- list");
+    Serial.println("- delete [name]");
+    Serial.println("- restart");
+    Serial.println();
+
+    showHelp = false;
+  }
+  String input = readSerialCommand();
+
+  if (input == "restart") {
+    rp2040.reboot();
+  } else if(input.startsWith("record ")) {
+    input.remove(0, 7);
+    Serial.print("\"");
+    Serial.print(input);
+    Serial.println("\"");
+  } else {
+    Serial.println("Invalid command!");
+    Serial.println();
+
+    showHelp = true;
+  }
+}
+
+String readSerialCommand() {
+  Serial.print("> ");
+  String input = "";
+  while (Serial) {
+    if (!Serial.available()) {
+      delay(10);
+      continue;
+    }
+
+    char c = Serial.read();
+    if (c == '\r') {
+      continue;
+    } else if (c == '\n') {
+      Serial.print("\n");
+      break;
+    } else if (c == '\b') {
+      input.remove(input.length() - 1);
+      Serial.println();
+      Serial.print("> ");
+      Serial.print(input);
+
+      continue;
+    }
+
+    input += c;
+    Serial.print(c);
+  }
+
+  return input;
 }
